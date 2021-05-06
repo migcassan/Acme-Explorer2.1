12877d53280d43402aabba36c718496fee3e6cac
Miguel Castillo <miguelcastillo2689@gmail.com>
2021-05-05 20:25:01 -0400

    heroku fixes

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('acme-explorer.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+//Install express server
+const express = require('express');
+const path = require('path');
 
-server.use(middlewares);
-server.use(router);
-server.listen(port);
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/acme-explorer'));
+
+app.get('*', function(req,res) {
+  res.sendFile(path.join(__dirname + '/dist/acme-explorer/index.html'));
+});
+app.listen(process.env.PORT || 8080);
